263b4921394afabd4762cd7682dfbf6886f8315b
Amuzey <ivanovad97@gmail.com>
2022-07-06 02:30:33 +0500

    final version 3

--- LoginApp/ViewControllers/WelcomeViewController.swift
+++ LoginApp/ViewControllers/WelcomeViewController.swift
@@ -11,17 +11,26 @@ class WelcomeViewController: UIViewController {
     
     @IBOutlet weak var greetingLabel: UILabel!
     
-  
-    
     var name = ""
     var surname = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-   
-        
+        view.addVerticalGradientLayer(topColor: .systemCyan, bottomColor: .systemPurple)
         greetingLabel.text = "Добрый день, \(name) \(surname)!"
+    }
+}
+// MARK: - Set background color
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
     }
 }
 
